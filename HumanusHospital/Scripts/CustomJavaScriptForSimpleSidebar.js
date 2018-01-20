92c6114ee70a4ca740057b1998e9c4995ff18c6f
Sofie-PC\Sofie <sofie.hellstrom@yh.humanus.se>
2018-01-20 17:21:56 +0100

    Fixed navigation

--- HumanusHospital/Scripts/CustomJavaScriptForSimpleSidebar.js
+++ HumanusHospital/Scripts/CustomJavaScriptForSimpleSidebar.js
@@ -2,7 +2,7 @@
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
 });
-$("#menu-toggle-2").click(function (e) {
+$("#menu-toggle-2").click(function(e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled-2");
     $('#menu ul').hide();
